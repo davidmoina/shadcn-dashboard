8edc602dfcf97bb3c6f4b95c0bd41f8973f4fee9
David Moina <luisdavidmoina@gmail.com>
2024-11-30 22:15:05 +0100

    💄 Add checkbox

---
+++ src/app/dashboard/checkbox/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
+
+export default function CheckboxPage() {
+  const [terms, setTerms] = useState(false);
+
+  return (
+    <div className="items-top flex space-x-2">
+      <Checkbox
+        id="terms1"
+        checked={terms}
+        onCheckedChange={(value) => setTerms(!!value)}
+      />
+      <div className="grid gap-1.5 leading-none">
+        <label
+          htmlFor="terms1"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Accept terms and conditions
+        </label>
+        <p className="text-sm text-muted-foreground">
+          You agree to our Terms of Service and Privacy Policy.
+        </p>
+
+        <div>
+          {terms ? (
+            <Badge variant="success" capitalize>
+              accepted
+            </Badge>
+          ) : (
+            <Badge variant="destructive" capitalize>
+              declined
+            </Badge>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
